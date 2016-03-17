c212f4dd8860973e70b2837d0bf3d82173f20dd5
glejeune <gregoire.lejeune@free.fr>
2016-03-17 12:46:25 +0100

    Update elixir + doc

--- _doc/api.md
+++ _doc/api.md
@@ -28,21 +28,53 @@
 
 ## assert_not_equal/2
 
+## assert_response_ok/1
+
+## assert_response_code/2
+
+## assert_response_not_code/2
+
+## assert_response_has_body/1
+
+## assert_response_not_has_body/1
+
+## assert_response_redirect/1
+
+## assert_response_not_found/1
+
+## assert_response_header/2
+
 ## assert_request_ok/1
+
+*Deprecated, use assert_response_ok/2*
 
 ## assert_request_code/2
 
+*Deprecated, use assert_response_code/2*
+
 ## assert_request_not_code/2
+
+*Deprecated, use assert_response_not_code/2*
 
 ## assert_request_has_body/1
 
+*Deprecated, use assert_response_has_body/1*
+
 ## assert_request_not_has_body/1
+
+*Deprecated, use assert_response_not_has_body/1*
 
 ## assert_request_redirect/1
 
+*Deprecated, use assert_response_redirect/1*
+
 ## assert_request_not_found/1
 
+*Deprecated, use assert_response_not_found/1*
+
 ## assert_request_header/2
+
+*Deprecated, use assert_response_header/2*
 
 ## debug/1
 
--- lib/Wok.Tests.ex
+++ lib/Wok.Tests.ex
@@ -3,20 +3,35 @@
 # Using mix.mk (https://github.com/botsunit/mix.mk)
 # MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
 defmodule Wok.Tests do
+	def unquote(:"produce")(arg1, arg2, arg3) do
+		:erlang.apply(:"wok_tests", :"produce", [arg1, arg2, arg3])
+	end
 	def unquote(:"produce")(arg1, arg2, arg3, arg4) do
 		:erlang.apply(:"wok_tests", :"produce", [arg1, arg2, arg3, arg4])
 	end
 	def unquote(:"produce")(arg1, arg2, arg3, arg4, arg5, arg6) do
 		:erlang.apply(:"wok_tests", :"produce", [arg1, arg2, arg3, arg4, arg5, arg6])
 	end
+	def unquote(:"produce")(arg1, arg2, arg3, arg4, arg5) do
+		:erlang.apply(:"wok_tests", :"produce", [arg1, arg2, arg3, arg4, arg5])
+	end
 	def unquote(:"build_message")(arg1) do
 		:erlang.apply(:"wok_tests", :"build_message", [arg1])
+	end
+	def unquote(:"request")(arg1, arg2) do
+		:erlang.apply(:"wok_tests", :"request", [arg1, arg2])
 	end
 	def unquote(:"request")(arg1, arg2, arg3) do
 		:erlang.apply(:"wok_tests", :"request", [arg1, arg2, arg3])
 	end
+	def unquote(:"request")(arg1, arg2, arg3, arg4, arg5) do
+		:erlang.apply(:"wok_tests", :"request", [arg1, arg2, arg3, arg4, arg5])
+	end
 	def unquote(:"request")(arg1, arg2, arg3, arg4, arg5, arg6) do
 		:erlang.apply(:"wok_tests", :"request", [arg1, arg2, arg3, arg4, arg5, arg6])
+	end
+	def unquote(:"follow")(arg1, arg2) do
+		:erlang.apply(:"wok_tests", :"follow", [arg1, arg2])
 	end
 	def unquote(:"follow")(arg1, arg2, arg3) do
 		:erlang.apply(:"wok_tests", :"follow", [arg1, arg2, arg3])
@@ -56,6 +71,30 @@ defmodule Wok.Tests do
 	end
 	def unquote(:"assert_request_header")(arg1, arg2) do
 		:erlang.apply(:"wok_tests", :"assert_request_header", [arg1, arg2])
+	end
+	def unquote(:"assert_response_ok")(arg1) do
+		:erlang.apply(:"wok_tests", :"assert_response_ok", [arg1])
+	end
+	def unquote(:"assert_response_code")(arg1, arg2) do
+		:erlang.apply(:"wok_tests", :"assert_response_code", [arg1, arg2])
+	end
+	def unquote(:"assert_response_not_code")(arg1, arg2) do
+		:erlang.apply(:"wok_tests", :"assert_response_not_code", [arg1, arg2])
+	end
+	def unquote(:"assert_response_has_body")(arg1) do
+		:erlang.apply(:"wok_tests", :"assert_response_has_body", [arg1])
+	end
+	def unquote(:"assert_response_not_has_body")(arg1) do
+		:erlang.apply(:"wok_tests", :"assert_response_not_has_body", [arg1])
+	end
+	def unquote(:"assert_response_redirect")(arg1) do
+		:erlang.apply(:"wok_tests", :"assert_response_redirect", [arg1])
+	end
+	def unquote(:"assert_response_not_found")(arg1) do
+		:erlang.apply(:"wok_tests", :"assert_response_not_found", [arg1])
+	end
+	def unquote(:"assert_response_header")(arg1, arg2) do
+		:erlang.apply(:"wok_tests", :"assert_response_header", [arg1, arg2])
 	end
 	def unquote(:"debug")(arg1, arg2) do
 		:erlang.apply(:"wok_tests", :"debug", [arg1, arg2])
