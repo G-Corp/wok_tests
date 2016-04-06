fc61bb007772bfabec239521afcb28357a8e8ce2
glejeune <gregoire.lejeune@free.fr>
2016-04-06 14:52:22 +0200

    Prepare 0.0.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Wok.Tests.Mixfile do
 
 	def project do
 		[app: :wok_tests,
-		 version: "0.0.1",
+		 version: "0.0.5",
 		 elixir: "~> 1.2",
 		 build_embedded: Mix.env == :prod,
 		 start_permanent: Mix.env == :prod,
