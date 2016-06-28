e22658ddd62cb237578a99fee8cc3c891a3daaea
glejeune <gregoire.lejeune@free.fr>
2016-06-28 15:29:17 +0200

    Prepare 0.2.1

--- mix.exs
+++ mix.exs
@@ -21,10 +21,10 @@ defmodule Wok.Tests.Mixfile do
 
   defp deps do
     [
-      {:bucs, git: "https://github.com/botsunit/bucs.git", tag: "0.0.2"},
+      {:bucs, "~> 0.1.0"},
       {:wok_http_adapter, git: "git@gitlab.botsunit.com:msaas/wok_http_adapter.git", tag: "0.1.0"},
-      {:wok_message_handler, git: "git@gitlab.botsunit.com:msaas/wok_message_handler.git", tag: "0.3.0"},
-      {:doteki, git: "https://github.com/botsunit/doteki.git", tag: "0.1.0"},
+      {:wok_message_handler, git: "git@gitlab.botsunit.com:msaas/wok_message_handler.git", tag: "0.4.0"},
+      {:doteki, "~> 0.1.0"},
       {:hackney, "~> 1.6.0"}    
     ]
   end
